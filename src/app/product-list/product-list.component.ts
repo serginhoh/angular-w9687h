01104075cbc65b4c4e3828b9ea9ff61f0904c94e
serginhoh <serginhoh@gmail.com>
2020-02-27 23:12:03 -0300

    3 files modified, 6 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,11 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotify(){
+    window.alert('The product will be on sale.')
+  }
+
 }
 
 
